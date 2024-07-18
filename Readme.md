8532b1ae9f2e135eb81fb5f5861d0b44d2db30f7
8309h <harshalwagh201718@gmail.com>
2024-07-18 13:34:15 +0530

    deploy

---
+++ Readme.md
@@ -0,0 +1,5 @@
+Video Explanation link : https://drive.google.com/file/d/1bJ7_TaE0A_ak_idahoS2iBnMarqPuj6C/view?usp=sharing
+deploy link: https://frontend-j69yiy1r6-8309hs-projects.vercel.app/
+
+
+
--- construction-project/routes/taskRoutes.js
+++ construction-project/routes/taskRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Task = require('../models/Task');
+const Task = require('../model/taskModel');
 
 // Calculate the end date based on start date and duration
 const calculateEndDate = (startDate, duration) => {
